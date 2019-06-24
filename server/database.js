a71d4047a12dda92a5e5b76d11c11a99a1f8aaf2
Phavuk <43843255+Phavuk@users.noreply.github.com>
2019-06-24 14:22:37 +0200

    server

--- server/database.js
+++ server/database.js
@@ -1,20 +1,544 @@
-    const mysql = require('mysql');
+const mysql = require('mysql');
+const TokenGenerator = require('uuid-token-generator');
+const tokgen = new TokenGenerator(); 
+
+let tokens = [];
+
+const getLogin = (login,password,callback) => {
+console.log(login,password);
+	const con = mysql.createConnection({
+	
+	host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+    port: "3306"
+
+	});
+
+	con.connect(function(err){
+		if(err) throw err 
+		let sql = "SELECT id,login,password FROM loginclient "+"WHERE login LIKE '"+login+"' "+"AND password like md5('"+password+"');";
+		console.log("It works");
+		con.query(sql,(err,result) => {	 
+		if(err) throw err; 
+			if(result.length==0){
+				console.log("User"+login+' is not defined');
+				let rslt = null;
+				callback(rslt);
+			}else{
+				console.log("User"+login+" is in the database");
+				let token = tokgen.generate();
+				let obj= new Object();
+				obj.login=login;
+				obj.token= token;
+				tokens=tokens.filter(person => person.login != login);
+				tokens.push(obj);
+				console.log(tokens);
+				let newresult = JSON.stringify(obj);
+				callback(newresult);
+			}
+		});
+	});
+}
+
+const getLogout = (login,token,callback) => {
+if(tokens.find(person => (person.login == login && person.token==token))){
+    tokens=tokens.filter(person => (person.login != login && person.token !=token));
+    callback(200);
+    console.log(tokens);
+  }
+  else{
+    callback(401);
+    console.log(tokens);
+  }
+
+}
+
+
+const getUserInfo = (login,token,callback) => {
+	const con = mysql.createConnection({
+
+	host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+    port: "3306"
+
+	});
+
+	con.connect(function(err){
+		if(err) throw err 
+		let sql = "select client.fname,client.lname,client.email from client inner join loginclient on loginclient.idc = client.id where login like'"+login+"';";
+		console.log("It works");
+		con.query(sql,(err,result) => {	 
+		if(err) throw err; 
+			if(result.length==0){
+				console.log("User"+login+' is not defined');
+
+				let rslt = null;
+				callback(rslt);
+			}else{
+
+				console.log("User"+login+" is in the database");
+				let obj= new Object();
+				obj.FirstName=result[0].fname;
+				obj.LastName=result[0].lname;
+				obj.Mail=result[0].email;
+				obj.ID=result[0].id;
+				let newresult = JSON.stringify(obj);
+				console.log(newresult);
+				callback(newresult);
+			}
+		});
+	});
+}
+
+
+const getAccounts = (login,token,callback) => {
+	const con = mysql.createConnection({
+
+	host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+    port: "3306"
+
+	});
+
+	con.connect(function(err){
+		if(err) throw err 
+		let sql = "SELECT AccNum,Amount from account INNER JOIN client on client.id=account.idc inner join loginclient on client.id=loginclient.idc where login like '"+login+"';";
+		console.log("It works");
+		con.query(sql,(err,result) => {	 
+		if(err) throw err; 
+			if(result.length==0){
+				console.log("User"+login+' is not defined');
+				let rslt = null;
+				callback(rslt);
+			}else{
+				console.log("User"+login+" is in the database");
+				let newresult = JSON.stringify(result);
+				console.log(newresult);
+				callback(newresult);
+			}
+		});
+	});
+}
+
+
+const getAccInfo = (login,token,accNum,callback) => {
+	const con = mysql.createConnection({
+
+	host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+    port: "3306"
+
+	});
+
+	let objGetted = new Object();
+	objGetted.login=login;
+	objGetted.token=token;
+	console.log(objGetted);
+	console.log("It works"+"	"+objGetted.login+"	"+objGetted.token);
+
+	for(let i=0;i<tokens.length;i++){
+		console.log("it work")
+		if(tokens[i].login==objGetted.login && tokens[i].token==objGetted.token){
+			con.connect(function(err){
+			if(err) throw err 
+			let sql = "select id,amount from account where AccNum like '"+accNum+"';";
+			con.query(sql,(err,result) => {	 
+			if(err) throw err; 
+				if(result.length==0){
+					console.log("Account is not in the database");
+					let rslt = null;
+					callback(rslt);
+				}else{
+					console.log("Account is in the database");
+					let newresult = JSON.stringify(result);
+					console.log(newresult);
+					callback(newresult);
+				}
+			});
+		});
+
+		break;
+
+		}else{
+			console.log("bad credentials");
+			let newresult = null;
+			callback(newresult);
+			break;
+		}
+
+	}
+}
+
+const getTransHistory = (login,idAcc,token,callback) => {
+	const con = mysql.createConnection({
+
+	host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+    port: "3306"
+
+	});
+
+	let objGetted = new Object();
+	objGetted.login=login;
+	objGetted.token=token;
+	console.log(objGetted);
+	console.log("It works"+"	"+objGetted.login+"	"+objGetted.token);
+
+	for(let i=0;i<tokens.length;i++){
+		if(tokens[i].login==objGetted.login && tokens[i].token==objGetted.token){
+			con.connect(function(err){
+			if(err) throw err 
+			let sql = "select * from transaction where RecAccount like '"+idAcc+"';";
+			con.query(sql,(err,result) => {	 
+			if(err) throw err; 
+				if(result.length==0){
+					console.log("Account is not in the database");
+					let rslt = null;
+					callback(rslt);
+				}else{
+					console.log("Account is in the database");
+					let newresult = JSON.stringify(result);
+					console.log(newresult);
+					callback(newresult);
+				}
+			});
+		});
+
+		break;
+
+		}else{
+			console.log("bad credentials");
+			let newresult = null;
+			callback(newresult);
+			break;
+		}
+
+	}
+}
+
+
+
+const getCards = (login,idAcc,token,callback) => {
+	const con = mysql.createConnection({
+
+	host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+	port: "3306"
+		
+	});
+
+	let objGetted = new Object();
+	objGetted.login=login;
+	objGetted.token=token;
+	console.log(objGetted);
+	console.log("It works"+"	"+objGetted.login+"	"+objGetted.token);
+
+	for(let i=0;i<tokens.length;i++){
+		if(tokens[i].login==objGetted.login && tokens[i].token==objGetted.token){
+			con.connect(function(err){
+			if(err) throw err 
+			let sql = "select id,expirem,expirey from card where ida like'"+idAcc+"';";
+			con.query(sql,(err,result) => {	 
+			if(err) throw err; 
+				if(result.length==0){
+					console.log("Account is not in the database");
+					let rslt = null;
+					callback(rslt);
+				}else{
+					console.log("Account is in the database");
+					let newresult = JSON.stringify(result);
+					console.log(newresult);
+					callback(newresult);
+				}
+			});
+		});
+
+		break;
+
+		}else{
+			console.log("bad credentials");
+			let newresult = null;
+			callback(newresult);
+			break;
+		}
+
+	}
+}
+
+
+const getCardInfo = (login,idCard,token,callback) => {
+	const con = mysql.createConnection({
+
+		host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+    port: "3306"
+
+	});
+
+	let objGetted = new Object();
+	objGetted.login=login;
+	objGetted.token=token;
+	console.log(objGetted);
+	console.log("It works"+"	"+objGetted.login+"	"+objGetted.token);
+
+	for(let i=0;i<tokens.length;i++){
+		if(tokens[i].login==objGetted.login && tokens[i].token==objGetted.token){
+			con.connect(function(err){
+			if(err) throw err 
+			let sql = "select Active,ExpireM,ExpireY from card where id like'"+idCard+"';";
+			con.query(sql,(err,result) => {	 
+			if(err) throw err; 
+				if(result.length==0){
+					console.log("Account is not in the database");
+					let rslt = null;
+					callback(rslt);
+				}else{
+					console.log("Account is in the database");
+					let newresult = JSON.stringify(result);
+					console.log(newresult);
+					callback(newresult);
+				}
+			});
+		});
+
+		break;
+
+		}else{
+			console.log("bad credentials");
+			let newresult = null;
+			callback(newresult);
+			break;
+		}
+
+	}
+}
+
+
+const getCardTransaction = (login,idCard,token,callback) => {
+	const con = mysql.createConnection({
+
+	host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+    port: "3306"
+
+	});
+
+	let objGetted = new Object();
+	objGetted.login=login;
+	objGetted.token=token;
+	console.log(objGetted);
+	console.log("It works"+"	"+objGetted.login+"	"+objGetted.token);
+
+	for(let i=0;i<tokens.length;i++){
+		if(tokens[i].login==objGetted.login && tokens[i].token==objGetted.token){
+			con.connect(function(err){
+			if(err) throw err 
+			let sql = "select * from cardtrans where idCard like'"+idCard+"';";
+			con.query(sql,(err,result) => {	 
+			if(err) throw err; 
+				if(result.length==0){
+					console.log("Account is not in the database");
+					//return null;
+					let rslt = null;
+					callback(rslt);
+				}else{
+					// {"FirstName","LastName","Mail","ID"}
+					console.log("Account is in the database");
+					let newresult = JSON.stringify(result);
+					console.log(newresult);
+					callback(newresult);
+				}
+			});
+		});
+
+		break;
+
+		}else{
+			console.log("bad credentials");
+			let newresult = null;
+			callback(newresult);
+			break;
+		}
+
+	}
+}
+
+const getChangePass = (login,token,oldpassword,newpassword,callback) => {
+	const con = mysql.createConnection({
+
+	host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+    port: "3306"
+
+	});
+
+	let objGetted = new Object();
+	objGetted.login=login;
+	objGetted.token=token;
+	console.log(objGetted);
+	console.log("It works"+"	"+objGetted.login+"	"+objGetted.token);
+	console.log(login+"	"+token+" "+oldpassword+" "+newpassword);
+
+	for(let i=0;i<tokens.length;i++){
+		if(tokens[i].login==objGetted.login && tokens[i].token==objGetted.token){
+			con.connect(function(err){
+			if(err) throw err 
+			let sql = "update loginclient set password = md5('"+newpassword+"') where login like '"+login+"' and password like md5('"+oldpassword+"');";
+			con.query(sql,(err,result) => {	 
+			if(err) throw err; 
+				if(result.affectedRows == 0){
+					console.log("Bad old password");
+					let rslt = null;
+					callback(rslt);
+				}else{
+					console.log(result.affectedRows);
+					console.log("Account is in the database");
+					let newresult = JSON.stringify("Password is changed");
+					console.log(newresult);
+					callback(newresult);
+
+				}
+			});
+		});
+
+		break;
+
+		}else{
+			console.log("bad credentials");
+			let newresult = null;
+			callback(newresult);
+			break;
+		}
+
+	}
+}
+
+
+const getBlockCard = (login,token,idCard,callback) => {
+	const con = mysql.createConnection({
+
+	host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+    port: "3306"
+
+	});
+
+	let objGetted = new Object();
+	objGetted.login=login;
+	objGetted.token=token;
+	console.log(objGetted);
+	console.log("It works"+"	"+objGetted.login+"	"+objGetted.token);
+
+	for(let i=0;i<tokens.length;i++){
+		if(tokens[i].login==objGetted.login && tokens[i].token==objGetted.token){
+			con.connect(function(err){
+			if(err) throw err 
+			let sql = "update card set Active = '"+0+"' where id like '"+idCard+"';";
+			con.query(sql,(err,result) => {	 
+			if(err) throw err; 
+				if(result.length==0){
+					console.log("Account is not in the database");
+					let rslt = null;
+					callback(rslt);
+				}else{
+					console.log("Account is in the database");
+					let newresult = JSON.stringify("Card blocked");
+					console.log(newresult);
+					callback(newresult);
+				}
+			});
+		});
+
+		break;
+
+		}else{
+			console.log("bad credentials");
+			let newresult = null;
+			callback(newresult);
+			break;
+		}
+
+	}
+}
+
+
+const sendCash = (login, idacc, token, recipient, amount, callback) => {
+	
+	const con = mysql.createConnection({
+
+	host: "itsovy.sk",
+    user: "glbank",
+    password: "password",
+    database: "glbank",
+    port: "3306"
+
+	});
     
-    const con = mysql.createConnection({
-        host: "localhost",
-        user:"root",
-        password:"",
-        database:"glbank"
-    });
+    let sql1 = "update account set account.amount = account.amount -"+amount+" where account.id like " + idacc;
+    let sql2 = "update account set account.amount = account.amount +"+amount+" where account.accnum like '" + recipient + "'";
+    let sql3 = "insert into transaction (idacc, idemployee, recaccount, transamount) values (" + idacc +", " + null + ", " + recipient + ", " + amount + ")";
 
-    con.connect(function(err){
-        if(err) throw err;
-        console.log("Database connected!");
-    });
-
-    module.exports = {
-        getLogin(username,password,queryResult)
-        {
-            const query = "select * from loginclient where login =\'" + username + "\' and password = MD5(\'" + password + "'\);";
-        }
+    if(tokens.find(person => (person.login == login && person.token==token)))
+    {
+        con.beginTransaction(function(err) {
+            if(err) {throw err; }
+            con.query(sql1, function(err, result){
+                if (err){
+                    con.rollback(function() {
+                        throw err;
+                    });
+                }
+                con.query(sql2, function(err, result){
+                    if (err){
+                        con.rollback(function() {
+                            throw err;
+                        });
+                    }
+                });
+                    con.query(sql3, function(err, result){
+                        if (err){
+                            con.rollback(function() {
+                                throw err;
+                            });
+                        }
+                    });
+    
+                        con.commit(function(err){
+                            let mess = new Object();
+                            mess.message = "Transaction Completed";
+                            callback(200, JSON.stringify(mess));
+    
+                        });
+            });
+        });
     }
+    else {
+        let mess=new Object;
+            mess.message="Something went wrong";
+            callback(401,JSON.stringify(mess));
+    }
+
+};
+
+
+module.exports = {getLogin,getLogout,getUserInfo,getAccounts,getAccInfo,getTransHistory,getCards,getCardInfo,getCardTransaction,getChangePass,getBlockCard,sendCash};
---
+++ server/index.js
@@ -0,0 +1,251 @@
+let database = require('./database');
+const app = require('express')();
+const bodyParser = require("body-parser");
+
+var cors = require('cors');
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });  
+
+//funguje
+app.post('/login',function(req,res,callback){
+
+	console.log("Login Service");
+
+	let login = req.body.login;
+	let password = req.body.password;
+
+	if(login=="" || password==""){
+	res.status(401).send("Error bad login");
+	}
+
+	database.getLogin(login,password,function(result) {
+
+	if(result==null){
+		console.log("null test");
+		res.status(403).send("Error login account not found");
+	}else{
+		res.status(200).send(result);
+	}
+
+	});
+
+});
+
+//funguje
+app.post('/logout',function(req,res){
+
+	let login = req.body.login;
+	let token = req.body.token;
+
+	//res.status(200).send();
+	database.getLogout(login,token,function(result){
+		console.log(result);
+		res.status(result).send("");
+	});
+
+});
+
+
+//funguje
+app.post('/userinfo',function(req,res){
+
+	let login = req.body.login;
+	let token = req.body.token;
+
+	//res.status(200).send();
+	database.getUserInfo(login,token,function(result){
+		res.status(200).send(result);
+	});
+
+});
+
+//funguje
+app.post('/accounts',function(req,res){
+
+	let login = req.body.login;
+	let token = req.body.token;
+
+	//res.status(200).send();
+	database.getAccounts(login,token,function(result){
+		if(result==null){
+			res.status(400).send("Wrong credentials");
+		}else{
+			res.status(200).send(result);
+		}
+	});
+});
+
+//funguje
+app.post('/accinfo',function(req,res){
+
+	let login = req.body.login;
+	let token = req.body.token;
+	let accNum = req.body.accNum;
+
+	//res.status(200).send();
+	database.getAccInfo(login,token,accNum,function(result){
+
+		if(result==null){
+			res.status(400).send("Wrong credentials");
+		}else{
+			res.status(200).send(result);
+		}
+	});
+
+});
+
+//funguje
+app.post('/transhistory',function(req,res){
+
+	let login = req.body.login;
+	let idAcc = req.body.idAcc;
+	let token = req.body.token;
+
+	//res.status(200).send();
+	database.getTransHistory(login,idAcc,token,function(result){
+
+		if(result==null){
+			res.status(400).send("Wrong credentials");
+		}else{
+			res.status(200).send(result);
+		}
+	});
+
+});
+
+//funguje
+app.post('/cards',function(req,res){
+
+	let login = req.body.login;
+	let idAcc = req.body.idAcc;
+	let token = req.body.token;
+
+	//res.status(200).send();
+	database.getCards(login,idAcc,token,function(result){
+
+		if(result==null){
+			res.status(400).send("Wrong credentials");
+		}else{
+			res.status(200).send(result);
+		}
+	});
+
+});
+
+//funguje
+app.post('/cardinfo',function(req,res){
+
+	let login = req.body.login;
+	let idCard = req.body.idCard;
+	let token = req.body.token;
+
+	//res.status(200).send();
+	database.getCardInfo(login,idCard,token,function(result){
+
+		if(result==null){
+			res.status(400).send("Wrong credentials");
+		}else{
+			res.status(200).send(result);
+		}
+	});
+
+});
+
+//funguje
+app.post('/cardtrans',function(req,res){
+
+	let login = req.body.login;
+	let idCard = req.body.idCard;
+	let token = req.body.token;
+
+	//res.status(200).send();
+	database.getCardTransaction(login,idCard,token,function(result){
+
+		if(result==null){
+			res.status(400).send("Wrong credentials");
+		}else{
+			res.status(200).send(result);
+		}
+	});
+
+});
+
+/*
+/changePassword 
+prijma: JSON{"username","token","oldpassword","newpassword"}
+vracia: 200 -
+	401 - JSON
+  403 -
+  
+/blockcard
+prijma: JSON{"username","token","idCard"}
+vracia: 200 
+  401 - JSON{"wrong token"}
+  403 - nenajdena karta
+*/
+
+app.post('/changepassword',function(req,res){
+
+	let login = req.body.login;
+	let token = req.body.token;
+	let oldpassword = req.body.oldpassword;
+	let newpassword = req.body.newpassword;
+
+	//res.status(200).send();
+	database.getChangePass(login,token,oldpassword,newpassword,function(result){
+
+		if(result==null){
+			res.status(400).send("Bad Old Password");
+		}else{
+			res.status(200).send(result);
+		}
+	});
+
+});
+
+
+app.post('/blockcard',function(req,res){
+
+	let login = req.body.login;
+	let token = req.body.token;
+	let idCard = req.body.idCard;
+
+	//res.status(200).send();
+	database.getBlockCard(login,token,idCard,function(result){
+
+		if(result==null){
+			res.status(400).send("Wrong credentials");
+		}else{
+			res.status(200).send(result);
+		}
+	});
+
+});
+
+app.post('/sendcash', (req, res) => {
+
+    let login = req.body.login;
+    let idacc = req.body.idacc;
+    let token = req.body.token;
+    let recipient = req.body.recipient;
+    let amount = req.body.amount;
+
+    if (login==undefined || idacc==undefined || token==undefined ||recipient==undefined
+        || amount==undefined){
+        res.status(401).send();
+    }
+    database.sendCash(login, idacc, token, recipient, amount, function(value, status){
+        res.status(status).send(value);
+        console.log("it works");
+    });
+    
+});
+
+app.listen(3000);
